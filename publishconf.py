feabf6d805efb50c872a770817f68fd0eae55456
Hong-She Liang <starofrainnight@gmail.com>
2016-02-12 21:55:09 +0800

    Fixed test and publish configurations

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = ''
+SITEURL = 'https://starofrainnight.github.io/cht'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
